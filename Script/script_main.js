75c067a6a74099a6a4e8b8b7e3ad72e101060b09
Ruturaj Biswal <106529938+ruturajbiswal@users.noreply.github.com>
2023-02-24 19:53:40 +0530

    00101

--- Script/script_main.js
+++ Script/script_main.js
@@ -3,13 +3,15 @@
 
 change_width = (win_width) => {
     console.log(win_width);
-    var logoimg = document.getElementById("logo_img").style; var logo = document.getElementById("logo").style;
-        var headtxt = document.getElementById("headtxt").style;
-        var link1 = document.getElementById("link1").style;
-        var link2 = document.getElementById("link2").style;
-        var link3 = document.getElementById("link3").style;
-        var link4 = document.getElementById("link4").style;
-        var link5 = document.getElementById("link5").style;
+    const logoimg = document.getElementById("logo_img").style; const logo = document.getElementById("logo").style;
+        const headtxt = document.getElementById("headtxt").style;
+        const link1 = document.getElementById("link1").style;
+        const link2 = document.getElementById("link2").style;
+        const link3 = document.getElementById("link3").style;
+        const link4 = document.getElementById("link4").style;
+        const link5 = document.getElementById("link5").style;
+        const main_head=document.getElementById("head_bar").style;
+
     // for decreasing margin & text size
     if (win_width <= 652) {
         
@@ -28,10 +30,19 @@ change_width = (win_width) => {
         link3.fontSize = "1em"
         link4.fontSize = "1em"
         link5.fontSize = "1em"
+        main_head.height="5.5em"
 
        
-        if(win_width < 320)
-        {
+        if(win_width <= 320)
+{main_head.height="5.5em"
+logoimg.width = '3.5em'
+logo.width = '2em'
+headtxt.fontSize = "1.2em"
+link1.fontSize = "1em"
+link2.fontSize = "1em"
+link3.fontSize = "1em"
+link4.fontSize = "1em"
+link5.fontSize = "1em"
             link1.marginRight = "0.4em"
             link1.marginLeft = "0.4em"
             link2.marginRight = "0.4em"
