46ac5fcb4bf7d63792c1ebf7d0b0902b54672408
Ruturaj Biswal <azadpradhan1234@gmail.com>
2023-02-25 09:03:21 +0530

    1509

--- Script/script_main.js
+++ Script/script_main.js
@@ -11,6 +11,7 @@ change_width = (win_width) => {
         const link4 = document.getElementById("link4").style;
         const link5 = document.getElementById("link5").style;
         const main_head=document.getElementById("head_bar").style;
+        const contxt=document.getElementById("context").style;   
 
     // for decreasing margin & text size
     if (win_width <= 652) {
@@ -31,6 +32,9 @@ change_width = (win_width) => {
         link4.fontSize = "1em"
         link5.fontSize = "1em"
         main_head.height="5.5em"
+        contxt.left="4em"
+       
+       
 
        
         if(win_width <= 320)
